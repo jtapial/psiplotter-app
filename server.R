635f361fb4d9b3f890e780c0b3e05a05141ef7c5
Kevin Ha <kcha@users.noreply.github.com>
2015-02-24 23:20:05 -0500

    Initial import

---
+++ server.R
@@ -0,0 +1,75 @@
+library(shiny)
+library(psiplot)
+
+shinyServer(function(input, output) {
+
+  Data <- reactive({
+    if (is.null(input$file)) {
+      data <- psi
+    }  else {
+      data <- read.table(input$file$datapath, header=TRUE, sep="\t", 
+                         stringsAsFactor=FALSE)
+    }
+    return(data)
+  })
+  
+  Config <- reactive({
+    if (is.null(input$configfile)) {
+      cfg <- config
+    } else {
+      cfg <- read.table(input$configfile$datapath, header=TRUE, sep="\t", 
+                        stringsAsFactor=FALSE,
+                        comment.char="")
+    }
+    
+    if (input$noconfig) {
+      cfg <- NULL
+    }
+    return(cfg)
+  })
+  
+  Event <- reactive({
+    if (is.null(input$event)) {
+      return(NULL)
+    }
+    ev <- strsplit(input$event, split = ", ")[[1]] 
+    d <- Data()[which(Data()$EVENT == ev[2]),]
+    validate(need(nrow(d) == 1, "Can't find event"))
+    return(d)
+  })
+  
+  output$chart <- renderPlot({
+    
+    if (input$color == "black") {
+      col <- rep("black", (ncol(Data()) - 6)/2)
+    } else {
+      col <- NULL  
+    }
+    
+    event <- Event()
+    
+    # generate bins based on input$bins from ui.R
+    plot_event(event, config = Config(), 
+               errorbar = input$errorbars,
+               col = col,
+               gridlines = input$gridlines,
+               groupmean = input$groupmean,
+               lines = input$lines,
+               pch = as.numeric(input$pch),
+               ylim = input$ylim,
+               cex.pch = input$cex.pch,
+               cex.xaxis = 1,
+               cex.yaxis = 1,
+               cex.main = 1.3)
+
+  }, width = 900, height = 600)
+
+  output$inputdata <- renderDataTable({
+    Data()
+  })
+
+  output$configdata <- renderDataTable({
+    Config()
+  })
+
+})
---
+++ ui.R
@@ -0,0 +1,92 @@
+library(shiny)
+library(psiplot)
+
+version <- "0.0.1 alpha"
+  
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("VAST-TOOLS PSI Plotter"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      wellPanel(
+        helpText(HTML("<b>UPLOAD DATA</b>")),
+        fileInput(
+          "file", "To begin, upload your VAST-TOOLS input file.", 
+          multiple=FALSE,
+          accept = c('text/plain', 'text/tab-separated-values')
+        ),
+        fileInput(
+          "configfile", "Also, upload an optional config file", 
+          multiple=FALSE,
+          accept = c('text/plain', 'text/tab-separated-values')
+        ),
+        checkboxInput("noconfig", "Do not use config file")
+      ),
+      
+      conditionalPanel(
+        'input.dataset == "Plot"',
+        wellPanel(
+          helpText(HTML("<b>PLOT SETTINGS</b>")),
+                  
+          checkboxInput("groupmean", "Show group-specific averages"),
+          
+          checkboxInput("gridlines", "Show grid lines", value = TRUE),
+          
+          checkboxInput("errorbars", "Show error bars", value = TRUE),
+          
+          checkboxInput("lines", "Draw line connecting data points"),
+          
+          selectInput("pch", "Symbol", 0:25, selected = 20),
+          
+          radioButtons("color", "Set color",
+                       c("Config" = "config",
+                         "BW" = "black")),
+          
+          sliderInput("ylim", "Set y-axis range", min = 0, max = 100,
+                      value = c(0, 100), step = 1),
+          
+          sliderInput("cex.pch", "Data point size (i.e. cex.pch)", 
+                      min = 0.4, max = 2.5, value = 1, step = 0.1)
+          
+#           actionButton("go", "Update")
+        )
+      ),
+      
+      wellPanel(
+        helpText(HTML("<b>ABOUT</b>")),
+        HTML(paste('Source Code:', '<a href="https://github.com/kcha/psiplotter-app" target="_blank">GitHub</a>')),
+        HTML('<br/>'),
+        HTML(paste('Version:', version))
+      ),
+            
+      wellPanel(
+        helpText(HTML("<b>RELATED LINKS</b>")),
+        HTML('<a href="https://github.com/vastgroup/vast-tools" target="_blank">VAST-TOOLS</a>'),
+        HTML('<br/>'),
+        HTML('<a href="https://github.com/kcha/psiplot" target="_blank">psiplot</a>')
+      ),
+      
+      wellPanel(
+        helpText(HTML("<b>CONTACT</b>")),
+        HTML('Kevin Ha'),
+        HTML('<br/>'),
+        HTML('<a href="mailto:k.ha@mail.utoronto.ca" target="_blank">k.ha@mail.utoronto.ca</a>')
+      )
+    ),
+    mainPanel(
+      tabsetPanel(
+        id = 'dataset',
+        tabPanel('Plot',
+                 selectInput("event", "Select event",  
+                             paste(psi$GENE, psi$EVENT, sep = ", "), 
+                             width = "100%"),
+                 plotOutput('chart')),
+        tabPanel('Input Data', dataTableOutput('inputdata')),
+        tabPanel('Config', dataTableOutput('configdata')),
+        tabPanel('Usage', verbatimTextOutput('usage'))
+      )
+    )
+  )
+))
